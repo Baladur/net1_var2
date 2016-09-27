6dae24e8d6498a18781995de7f459290cd4b8660
Alexey Smagin <asblackpn@gmail.com>
2016-09-28 02:03:41 +0300

    server code is clean now

--- src/com/ipovselite/Server.java
+++ src/com/ipovselite/Server.java
@@ -16,10 +16,7 @@ public class Server {
         ServerSocket serv;
 
         serv = new ServerSocket(port, 0, InetAddress.getByName(host));
-
-
         Socket s = serv.accept();
-
 
         byte[] size = new byte[Long.BYTES];
         byte[] name = new byte[256];
@@ -36,7 +33,6 @@ public class Server {
         int read = 0;
         FileOutputStream fos = new FileOutputStream("download/" + sname);
         int totalRead = 0;
-        byte[] file = new byte[(int)lsize];
 
         while ((read = s.getInputStream().read(fileBuf)) > 0) {
             //use Math.min because in the last iteration we dont get correct count of bytes, we get 100 bytes somewhy
@@ -46,32 +42,8 @@ public class Server {
             System.out.println("read = " + read + ", totalRead = " + totalRead);
         }
 
-
-        //InputStream name = s.getInputStream();
-        /*byte buf[] = new byte[256];
-        int r = name.read(buf);
-        String nameStr = new String(buf, 0, r);*/
-
-        /*InputStream size = s.getInputStream();
-        byte buf2[] = new byte[256];
-        r = size.read(buf2);
-        String sizeStr = new String(buf2, 0, r);
-        int sizeVal = Integer.parseInt(sizeStr);;
-
-        InputStream is = s.getInputStream();
-        byte[] buffer = new byte[sizeVal];
-        r = is.read(buffer);
-        System.out.println(r+" "+sizeVal);
-        FileOutputStream fos=new FileOutputStream("download/"+nameStr);
-        fos.write(buffer, 0, sizeVal);*/
-
-        //is.close();
         s.close();
         fos.close();
-    }
-
-    public void go(){
-
     }
 
 }
