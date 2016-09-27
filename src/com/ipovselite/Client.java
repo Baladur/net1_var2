68b757f3f87463eead4a9a0368873fc964bfd0c6
Roman Yakupov <ryo.jacob@gmail.com>
2016-09-27 12:15:43 +0300

    Added portional file transfer

--- src/com/ipovselite/Client.java
+++ src/com/ipovselite/Client.java
@@ -2,6 +2,7 @@ package com.ipovselite;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,11 +15,11 @@ public class Client {
         String message = "";
 
         String host = "127.0.0.1";
-        int port = 49001;
-
+        int port = 12345;
+        Socket sock = null;
 
         try {
-            Socket sock = new Socket(host, port);
+            sock = new Socket(host, port);
 
             /*message = br.readLine();
             sock.getOutputStream().write(message.getBytes());
@@ -29,16 +30,40 @@ public class Client {
             String data = new String(buf, 0, r);
             System.out.println(data);*/
 
-            String actualFileName = "sharik.png";
+            String actualFileName = "Net1_var2.iml";
 
             Path path = Paths.get(actualFileName);
             byte[] data = Files.readAllBytes(path);
             String fileName = path.getFileName().toString();
-            sock.getOutputStream().write(fileName.getBytes());
+            File file = new File(fileName);
+            //this thing to read file by portions
+            BufferedReader fileReader = new BufferedReader(new FileReader(new File(fileName)));
+            //portion's size is 100 bytes (change it)
+            char[] buf = new char[100];
+            //send name of file
+            sock.getOutputStream().write(actualFileName.getBytes());
+            //send length of file
+            sock.getOutputStream().write(ByteBuffer.allocate(Long.BYTES).putLong(file.length()).array());
+            int read = 0;
+            //use Math.min because in the last iteration we have to pass less than 100 bytes
+            for (long i = 0; i < file.length(); i += Math.min(100, (int)(file.length() - i))) {
+
+                read = fileReader.read(buf);
+                sock.getOutputStream().write(new String(buf).getBytes());
+                System.out.println("Client:Sent " + (i + read));
+            }
+            /*sock.getOutputStream().write(fileName.getBytes());
             sock.getOutputStream().write(String.valueOf(data.length).getBytes());
-            sock.getOutputStream().write(data);
+            sock.getOutputStream().write(data);*/
 
         }
-        catch (IOException ex) {ex.printStackTrace();}
+        catch (IOException ex) {ex.printStackTrace();} finally {
+            try {
+                //don't forget to close fucken sockets
+                sock.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/com/ipovselite/Server.java
+++ src/com/ipovselite/Server.java
@@ -3,6 +3,7 @@ package com.ipovselite;
 import sun.misc.IOUtils;
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
 import java.nio.charset.*;
 import java.util.Scanner;
 
@@ -11,7 +12,7 @@ public class Server {
         System.out.println("Server");
 
         String host = "127.0.0.1";
-        int port = 49001;
+        int port = 12345;
         ServerSocket serv;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,14 +32,33 @@ public class Server {
         message = br.readLine();
         s.getOutputStream().write(message.getBytes());
         s.close();*/
+        byte[] size = new byte[Long.BYTES];
 
+        byte[] name = new byte[256];
+        int readName = s.getInputStream().read(name);
+        String sname = new String(name, 0, readName);
+        System.out.println(sname);
+        s.getInputStream().read(size);
+        long lsize = ByteBuffer.wrap(size).getLong();
+        System.out.println("size = " + lsize);
+        byte[] fileBuf = new byte[100];
+        int read = 0;
+        FileOutputStream fos = new FileOutputStream("download/" + sname);
+        int totalRead = 0;
+        byte[] file = new byte[(int)lsize];
 
-        InputStream name = s.getInputStream();
-        byte buf[] = new byte[256];
+        while ((read = s.getInputStream().read(fileBuf)) > 0) {
+            //use Math.min because in the last iteration we dont get correct count of bytes, we get 100 bytes somewhy
+            fos.write(fileBuf, 0, Math.min(read, (int)lsize - totalRead));
+            totalRead += Math.min(read, (int)lsize - totalRead);
+            System.out.println("read = " + read + ", totalRead = " + totalRead);
+        }
+        //InputStream name = s.getInputStream();
+        /*byte buf[] = new byte[256];
         int r = name.read(buf);
-        String nameStr = new String(buf, 0, r);
+        String nameStr = new String(buf, 0, r);*/
 
-        InputStream size = s.getInputStream();
+        /*InputStream size = s.getInputStream();
         byte buf2[] = new byte[256];
         r = size.read(buf2);
         String sizeStr = new String(buf2, 0, r);
@@ -49,9 +69,10 @@ public class Server {
         r = is.read(buffer);
         System.out.println(r+" "+sizeVal);
         FileOutputStream fos=new FileOutputStream("download/"+nameStr);
-        fos.write(buffer, 0, sizeVal);
+        fos.write(buffer, 0, sizeVal);*/
 
-        is.close();
+        //is.close();
+        s.close();
         fos.close();
     }
 
