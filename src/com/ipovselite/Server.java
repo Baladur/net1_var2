5da3a175298c274ef2bde6c0841d59ed441550e0
Roman Yakupov <ryo.jacob@gmail.com>
2016-10-02 15:47:56 +0300

    Added print of local ips to Server constructor

--- src/com/ipovselite/Server.java
+++ src/com/ipovselite/Server.java
@@ -8,6 +8,7 @@ import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.charset.*;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,7 +19,20 @@ public class Server {
     private List<ClientProcessor> clientProcessors = new ArrayList<>();
 
     public Server(int pPort, String pDownloadDir) throws UnknownHostException, IOException {
-        serverSocket = new ServerSocket(pPort);
+        System.out.println("Your Host addr: " + InetAddress.getLocalHost().getHostAddress());  // often returns "127.0.0.1"
+        Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
+        for (; n.hasMoreElements();)
+        {
+            NetworkInterface e = n.nextElement();
+
+            Enumeration<InetAddress> a = e.getInetAddresses();
+            for (; a.hasMoreElements();)
+            {
+                InetAddress addr = a.nextElement();
+                System.out.println("  " + addr.getHostAddress());
+            }
+        }
+        serverSocket = new ServerSocket(pPort, 0, InetAddress.getByName("172.18.25.184"));
     }
 
     public void shutDown() throws IOException {
